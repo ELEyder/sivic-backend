ea5a095824dc3dbe3207918cfde2298ce1364324
ELEyder <74992528+ELEyder@users.noreply.github.com>
2025-04-15 17:56:48 -0500

    Detalles finales

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\CasoController;
+use App\Http\Controllers\ContactoController;
 use App\Http\Controllers\HomePageController;
+use App\Http\Controllers\ImagenWebController;
 use App\Http\Controllers\InformationPageController;
 use App\Http\Controllers\StatisticsPageController;
 use Illuminate\Http\Request;
@@ -9,40 +12,38 @@ use Illuminate\Support\Facades\Route;
 
 Route::prefix('v1')->group(function () {
 
-    Route::get('casos/dashboard-data', [CasoController::class, 'dashboardData']);
-    Route::resource('casos', 'CasoController');
-    Route::put('casos/{id}', 'CasoController@update');
-    Route::delete('casos/{id}', 'CasoController@destroy');
-    
-    Route::resource('contactos', 'ContactoController');
-    Route::put('contactos/{id}', 'ContactoController@update');
-    Route::delete('contactos/{id}', 'ContactoController@destroy');
+    Route::post('login', [AuthController::class, 'login']);
+    Route::post('logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
 
     Route::resource('imagenes_web', 'ImagenWebController');
-    Route::put('imagenes_web', 'ImagenWebController@update');
-    Route::delete('imagenes_web/{id}', 'ImagenWebController@destroy');
-
     Route::get('home-page', [StatisticsPageController::class, 'index']);
-    Route::post('home-page', [StatisticsPageController::class, 'update']);
-
     Route::get('statistics-page', [StatisticsPageController::class, 'index']);
-    Route::post('statistics-page', [StatisticsPageController::class, 'update']);
-
     Route::get('information-page', [InformationPageController::class, 'index']);
-    Route::post('information-page', [InformationPageController::class, 'update']);
 
-    Route::put('test-upload', function (Request $request) {
-        return response()->json([
-            'method' => $request->method(),
-            'received_data' => $request->all(),
-            'files' => array_map(function ($file) {
-                return [
-                    'name' => $file->getClientOriginalName(),
-                    'size' => $file->getSize(),
-                    'mime' => $file->getMimeType()
-                ];
-            }, $request->allFiles()),
-            'php_input' => base64_encode(file_get_contents('php://input'))
-        ]);
+    Route::get('casos/dashboard-data', [CasoController::class, 'dashboardData']);
+
+    Route::post('casos', [CasoController::class, 'store']);
+    Route::post('contactos', [ContactoController::class, 'store']);
+
+    Route::middleware(['auth:sanctum', 'admin'])->group(function () {
+        Route::get('casos', [CasoController::class, 'index']);
+        Route::get('casos/{caso}', [CasoController::class, 'show']);
+        Route::put('casos/{id}', [CasoController::class, 'update']);
+        Route::delete('casos/{id}', [CasoController::class, 'destroy']);
+
+        Route::get('contactos', [ContactoController::class, 'index']);
+        Route::get('contactos/{contacto}', [ContactoController::class, 'show']);
+        Route::put('contactos/{id}', [ContactoController::class, 'update']);
+        Route::delete('contactos/{id}', [ContactoController::class, 'destroy']);
+
+        Route::put('imagenes_web', [ImagenWebController::class, 'update']);
+        Route::delete('imagenes_web/{id}', [ImagenWebController::class, 'destroy']);
+
+        Route::post('home-page', [StatisticsPageController::class, 'update']);
+
+        Route::post('statistics-page', [StatisticsPageController::class, 'update']);
+
+        Route::post('information-page', [InformationPageController::class, 'update']);
     });
+
 });
